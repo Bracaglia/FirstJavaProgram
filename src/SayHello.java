195f7fc2bb1f3ef5af9c5d9c37b0a940d70ffd0f
Bracaglia <shaws0598@gmail.com>
2022-09-26 13:47:24 -0700

    initial commit

---
+++ src/SayHello.java
@@ -0,0 +1,8 @@
+public class SayHello {
+    public void printMessage() {
+        String name = "Samantha";
+        String location = "Rosamond, CA";
+        int age = 23;
+        System.out.format("My name is %s, I am %d years old, and my hometown is %s", name, age, location);
+    }
+}
